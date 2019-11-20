a5b3faaa4047ee3635edad3bc5061124b52b212a
Thomas Monte <thomasmont3@gmail.com>
2019-11-19 23:58:05 -0700

    publish first version

--- src/optics.test.ts
+++ src/optics.test.ts
@@ -20,13 +20,13 @@ test('reducer with single update function applies that function to state', () =>
 })
 
 test('reducer with multiple update functions applies all functions to state', () => {
-    const state: TestState = {
-      name: 'bob'
-    }
-    const updateFn1 = (s: TestState) => ({name: `${s.name} marley`})
-    const updateFn2 = (s: TestState) => ({name: `${s.name.repeat(2)}`})
-    const updateFn3 = (s: TestState) => ({name: `${s.name} done`})
-    const expected = updateFn3(updateFn2(updateFn1(state)))
-    const actual = reducer(state, updateState([updateFn1, updateFn2, updateFn3]))
-    expect(actual).toEqual(expected)
-  })
+  const state: TestState = {
+    name: 'bob'
+  }
+  const updateFn1 = (s: TestState) => ({name: `${s.name} marley`})
+  const updateFn2 = (s: TestState) => ({name: `${s.name.repeat(2)}`})
+  const updateFn3 = (s: TestState) => ({name: `${s.name} done`})
+  const expected = updateFn3(updateFn2(updateFn1(state)))
+  const actual = reducer(state, updateState([updateFn1, updateFn2, updateFn3]))
+  expect(actual).toEqual(expected)
+})
