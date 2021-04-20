87ee0f15a0b412bcb2f7b49384dc0a277feaa36b
Josh DeGraw <josh-degraw@pluralsight.com>
2021-04-20 10:08:13 -0600

    Misc minor updates

--- src/optiqs.ts
+++ src/optiqs.ts
@@ -1,4 +1,4 @@
-import {AnyAction, Action, Reducer} from 'redux'
+import type {AnyAction, Action, Reducer} from 'redux'
 
 export type OptiqsStateUpdateFn<S> = (fn: S) => S
 
@@ -19,9 +19,18 @@ export interface OptiqsActionCreator {
 
 /**
  * Creates an action that will be used in the reducer to update state in the store.
- * @param payload A function or array of functions that will be used in the reducer.
+ * @param payload A function that will be used in the reducer.
+ * @returns An action with the given function as the payload.
+ */
+export function updateState<S>(payload: OptiqsStateUpdateFn<S>): OptiqsAction<S> 
+
+/**
+ * Creates an action that will be used in the reducer to update state in the store.
+ * @param payloads An array of functions that will be used in the reducer. They will be applied in the order they are declared.
  * @returns An action with the given functions as the payload.
  */
+export function updateState<S>(payloads: OptiqsStateUpdateFn<S>[]): OptiqsAction<S> 
+
 // Using named functions can help with debugging stack traces etc.
 export function updateState<S>(payload: OptiqsActionCreatorPayload<S>): OptiqsAction<S> {
   return {
@@ -40,20 +49,21 @@ const isOptiqsAction = (action: AnyAction): action is OptiqsAction<any> =>
 /**
  * Reducer used to handle Optiqs actions
  * @param state The current state.
- * @param action The dispatched action. You can optionally provide a type here to strongly type this reducer to only accept specific actiopns.
+ * @param action The dispatched action. You can optionally provide a type here to strongly type this reducer to only accept specific actions.
  * @returns The updated state.
  */
-export const reducer = <S, A extends Action = AnyAction>(state: S, action: A): S => {
-  return isOptiqsAction(action)
-    ? Array.isArray(action.payload)
+export function reducer<S, A extends Action = AnyAction>(state: S, action: A): S {
+  if (isOptiqsAction(action)) {
+    return Array.isArray(action.payload)
       ? action.payload.reduce((st, fn) => fn(st), state)
       : action.payload(state)
-    : state
+  }
+  return state
 }
 
 /**
  * Creates a reducer, seeding it with the provided initial state.
- * @param initialState The initial state of the reducer
+ * @param initialState The initial state of the reducer.
  * @returns The reducer.
  */
 export const createReducer = <S, A extends Action = AnyAction>(initialState: S): Reducer<S, A> =>
